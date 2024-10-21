6e02d4a2c81b9434f38efa19f6ddba4a9a814547
stk346 <stk346@gmail.com>
2024-10-22 01:20:14 +0900

    chore: 쓸모 없는 코드 제거

--- src/pages/login/KakaoLogin.tsx
+++ src/pages/login/KakaoLogin.tsx
@@ -15,5 +15,3 @@ const SocialKakao = () => {
 }
 
 export default SocialKakao;
-
-http://localhost:3000/kakaoAuth?code=lUMO72lyCrHuSLvGQU9gtVNB2ETEiN1aGkLCTcuB192d1nAOx8R7DgAAAAQKKiVTAAABkq_GY0vGDcCf5rkkeA
