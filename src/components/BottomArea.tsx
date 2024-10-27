6df41c6ea0014ace9fcd1d4958d5cc36852a85a1
stk346 <stk346@gmail.com>
2024-10-28 03:06:55 +0900

    feat: 카카오 로그인 버튼 기능 구현

--- src/components/BottomArea.tsx
+++ src/components/BottomArea.tsx
@@ -1,7 +1,7 @@
 
 export default function BottomArea() {
     return (
-        <div className="bg-red-300 flex flex-col items-center bottom-full h-60">
+        <div className="bg-red-300 flex flex-col items-center h-60">
             하단 영역입니다.
         </div>
     )
--- src/pages/login/KakaoLogin.tsx
+++ src/pages/login/KakaoLogin.tsx
@@ -1,3 +1,5 @@
+import Layout from "../../components/Layout";
+
 const KakaoLogin = () => {
     const restApiKey = process.env.REACT_APP_KAKAO_REST_API_KEY;
     const redirectUrl = "http://localhost:8082/api/v1/users/kakao/login/callback";
@@ -8,7 +10,17 @@ const KakaoLogin = () => {
     }
     return (
         <>
-            <button onClick={handleLogin}>카카오 로그인</button>
+            <Layout>
+                <div className="flex flex-col justify-center items-center h-96">
+                    <button onClick={handleLogin}>
+                        <img
+                            src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+                            width="200"
+                            alt="카카오 로그인 버튼"
+                        />
+                    </button>
+                </div>
+            </Layout>
         </>
     )
 }
