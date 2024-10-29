32a8c1375c38b775f133f4718d974ada8cb8e6ca
stk346 <stk346@gmail.com>
2024-10-30 03:47:32 +0900

    feat : 백엔드에서 edge-service 도입해서 각 애플리케이션과 swagger 의존성 제거

--- src/pages/login/KakaoLogin.tsx
+++ src/pages/login/KakaoLogin.tsx
@@ -2,7 +2,7 @@ import Layout from "../../components/Layout";
 
 const KakaoLogin = () => {
     const restApiKey = process.env.REACT_APP_KAKAO_REST_API_KEY;
-    const redirectUrl = "http://localhost:8082/api/v1/users/kakao/login/callback";
+    const redirectUrl = "http://localhost:8080/api/edge/users/kakao/login/callback";
 
     const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${restApiKey}&redirect_uri=${redirectUrl}&response_type=code`;
     const handleLogin = () => {
