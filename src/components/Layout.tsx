63aad57c7bb1fc954753ad97f7f594292b21da60
stk346 <stk346@gmail.com>
2024-12-11 01:27:48 +0900

    refactor: Layout.tsx - 레이아웃 요소들이 겹치지 않게 코드 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,14 +7,21 @@ import SideBar from "./menu/SideBar";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <div className="bg-orange-300 min-h-screen">
-                <HomeButton />
-                <SideBar />
-                {/* 탭 섹션 */}
-                <div className="flex flec-col justify-center space-x-4 py-4">
-                    <main>{children}</main>
-                </div>
-                <BottomArea />
+            <div className="bg-orange-300 min-h-screen flex flex-col">
+                <header className="flex items-center justify-between p-4">
+                    <HomeButton />
+                    <SideBar />
+                </header>
+
+                {/* 메인 콘텐츠 영역 */}
+                <main className="flex-grow flex flex-col items-center py-4 px-6">
+                    {children}
+                </main>
+
+                {/* 하단 영역 */}
+                <footer className="mt-auto">
+                    <BottomArea />
+                </footer>
             </div>
         </div>
     )
