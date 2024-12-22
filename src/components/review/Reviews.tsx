1481fcffe568d2a9c8d8d719ee8a4db2ef0331e9
stk346 <stk346@gmail.com>
2024-12-23 04:05:37 +0900

    refactor: 리뷰가 완전히 불러와 졌을 때만 다음 api 호출하도록 코드 수정

--- src/components/review/Reviews.tsx
+++ src/components/review/Reviews.tsx
@@ -7,7 +7,6 @@ import Review from "../../apis/user/interfaces/Review";
 import { useApiFetch } from "../../hooks/FetchApiFunc";
 import InfiniteScroll, { infiniteScrollHabndler } from "../infinitescroll/InfiniteScroll";
 import { createEmptyPage, Page } from "../../apis/movie/interfaces/Page";
-import { create } from "domain";
 
 interface ReviewsProps {
     code: number;
@@ -17,14 +16,8 @@ const Reviews: React.FC<ReviewsProps> = ({ code }) => {
     const [reviews, setReviews] = useState<Review[]>([]);
     const [page, setPage] = useState(1);
     const [hasMore, setHasMore] = useState(true);
-    const [isLoading, setIsLoading] = useState(true);
-    const size: number = 10;
 
-    // 리뷰 병합 및 정렬 함수 (중복 제거)
-    const mergeReviews = (prevReviews: Review[], newReviews: Review[]) => {
-        console.log('mergeReviews ', 'page: ', page - 1, 'isLoading: ', isLoading);
-        setReviews((prevReviews) => [...prevReviews, ...newReviews])
-    };
+    const size: number = 10;
 
     const addReview = async (newReview: string): Promise<void> => {
         const response = await UserMovieApiServiceSecure.addReview(code, newReview);
@@ -35,36 +28,27 @@ const Reviews: React.FC<ReviewsProps> = ({ code }) => {
         return UserMovieApiServicePublic.getReviews(code, page - 1, size);
     }, [code, page]);
 
-    const pagedData = useApiFetch<Page<Review[]>>(fetchReviews) || createEmptyPage<Review[]>();
+    const { data: pagedData, isLoading: reviewLoading } = useApiFetch<Page<Review[]>>(fetchReviews) || createEmptyPage<Review[]>();
 
     useEffect(() => {
-        console.log("pagedData.content.length: ", pagedData?.content.length, " page: ", page - 1);
-        if (pagedData.content.length > 0) {
-            console.log('pagedData', pagedData.content.map((review) => review.content));
-            setIsLoading(true);
-            setReviews((prevReviews) => [...prevReviews, ...pagedData.content]);
+        if (!reviewLoading) {
+            if (pagedData.content.length > 0) {
+                setReviews((prevReviews) => [...prevReviews, ...pagedData.content]);
+            }
         }
-        if (pagedData?.last) {
+        if (pagedData.last) {
             setHasMore(false);
         }
-        setIsLoading(false);
     }, [pagedData]);
 
-    // const loadMoreReviews = () => {
-    //     if (hasMore && !isLoading) {
-    //         setIsLoading(true);
-    //         setPage((prevPage) => prevPage + 1);
-    //     }
-    // };
-
+    // 무한 스크롤
     const setNextPage = useCallback<infiniteScrollHabndler>(() => {
-        console.log("setNextPage", ' isLoading: ',isLoading);
+        if (reviewLoading || !hasMore) return;
+
         if (hasMore) {
-            setIsLoading(true);
             setPage((prevPage) => prevPage + 1);
-            setIsLoading(false);
         }
-    }, []);
+    }, [reviewLoading]);
 
     return (
         <div className="container mx-auto">
@@ -74,7 +58,7 @@ const Reviews: React.FC<ReviewsProps> = ({ code }) => {
             <InfiniteScroll
                 onEnd={setNextPage}
                 rootMargin="50px"
-                isLoading={isLoading}
+                isLoading={reviewLoading}
             >
                 <ReviewList reviews={reviews} />
             </InfiniteScroll>
