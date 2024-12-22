e178385cca4566b1fa53e440263e4273ebaabfdf
stk346 <stk346@gmail.com>
2024-12-23 05:50:37 +0900

    refactor: 내 리뷰 페이지에서 무한스크롤 방식 변경 및 표시 날짜 생성일자로 변경

--- src/pages/my/movie/reviews/MyReviews.tsx
+++ src/pages/my/movie/reviews/MyReviews.tsx
@@ -4,6 +4,7 @@ import UserMovieApiServiceSecure from "../../../../apis/user/UserMovieApiService
 import { useApiFetch } from "../../../../hooks/FetchApiFunc";
 import Review from "../../../../apis/user/interfaces/Review";
 import { createEmptyPage, Page } from "../../../../apis/movie/interfaces/Page";
+import InfiniteScroll, { infiniteScrollHabndler } from "../../../../components/infinitescroll/InfiniteScroll";
 
 const MyReviews = () => {
     const [reviews, setReviews] = useState<Review[]>([]);
@@ -15,61 +16,64 @@ const MyReviews = () => {
         return UserMovieApiServiceSecure.getMyReviews(page - 1, size);
     }, [page]);
 
-    const { data: pagedReview, isLoading: reviewLoaded } = useApiFetch<Page<Review[]>>(fetchReviews) || createEmptyPage<Review[]>();
+    const { data: pagedReview, isLoading: reviewLoading } = useApiFetch<Page<Review[]>>(fetchReviews) || createEmptyPage<Review[]>();
     useEffect(() => {
-        if (pagedReview && pagedReview.content.length > 0) {
-            setReviews((prevReviews) => [...prevReviews, ...pagedReview.content]);
-            if (pagedReview.content.length < size) {
-                setHasMore(false);
+        if (!reviewLoading) {
+            if (pagedReview && pagedReview.content.length > 0) {
+                setReviews((prevReviews) => [...prevReviews, ...pagedReview.content]);
+                if (pagedReview.content.length < size) {
+                    setHasMore(false);
+                }
             }
         }
     }, [pagedReview]);
-
-    const handleScroll = () => {
-        const bottom = window.innerHeight + document.documentElement.scrollTop >= document.documentElement.scrollHeight;
-        if (bottom && hasMore) {
-            setPage((prevPage) => prevPage + 1);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [hasMore]);
 
     const handleTitleClick = (code: number) => {
         window.location.href = `/movie?code=${code}`;
     };
 
+    const setNextPage = useCallback<infiniteScrollHabndler>(() => {
+        if (reviewLoading || !hasMore) return;
+    
+        if (hasMore) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      }, [reviewLoading]);
+
     return (
         <Layout>
-            <div className="bg-white p-2 shadow-md w-full max-w-md border border-gray-400 mt-8">
-                <p className="text-lg">
-                    작성한 리뷰
-                </p>
-            </div>
-            <div className="bg-white p-2 shadow-md w-full max-w-md border border-gray-400">
-                <ul>
-                    {reviews && reviews.map((review, index) => (
-                        <li key={review.id}>
-                            <p
-                                className="truncate cursor-pointer text-blue-500 hover:underline hover:text-blue-700"
-                                onClick={() => { handleTitleClick(review.code) }}
-                            >
-                                {review.title}
-                            </p>
-                            <p>{review.content}</p>
-                            <p className="mb-3">작성일: {new Date(review.modifiedAt).toLocaleDateString()}</p>
-                            {/* 리뷰 사이에 줄 추가 */}
-                            {index < reviews.length - 1 && (
-                                <hr className="my-2 border-gray-300" />
-                            )}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <InfiniteScroll
+                onEnd={setNextPage}
+                rootMargin="50px"
+                isLoading={reviewLoading}
+            >
+                <div className="bg-white p-2 shadow-md w-full max-w-md border border-gray-400 mt-8">
+                    <p className="text-lg">
+                        작성한 리뷰
+                    </p>
+                </div>
+                <div className="bg-white p-2 shadow-md w-full max-w-md border border-gray-400">
+                    <ul>
+                        {reviews && reviews.map((review, index) => (
+                            <li key={review.id}>
+                                <p
+                                    className="truncate cursor-pointer text-blue-500 hover:underline hover:text-blue-700"
+                                    onClick={() => { handleTitleClick(review.code) }}
+                                >
+                                    {review.title}
+                                </p>
+                                <p>{review.content}</p>
+                                <p className="mb-3">작성일: {new Date(review.createdAt).toLocaleDateString()}</p>
+                                {/* 리뷰 사이에 줄 추가 */}
+                                {!reviewLoading && index < reviews.length - 1 && (
+                                    <hr className="my-2 border-gray-300" />
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </InfiniteScroll>
+
         </Layout>
     );
 }
