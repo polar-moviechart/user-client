4d086c216a841470ba5bce82e388bfd27b018044
stk346 <stk346@gmail.com>
2025-01-20 03:55:56 +0900

    refactor: HomeButton.tsx - 메인 페이지에서 홈 버튼 누를 때 새로고침 될 수 있도록 코드 수정

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,13 +1,27 @@
 "use client";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export default function HomeButton() {
+    const navigate = useNavigate(); // navigate 훅을 사용하여 페이지 이동
+    const location = useLocation();
+
+    const handleClick = () => {
+        if (location.pathname === "/") {
+            window.location.reload();
+        } else {
+            navigate("/");
+        }
+    };
+
     return (
         <div className="fixed top-4 left-4">
-            <Link to="/" className="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md font-semibold">
+            <button
+                className="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md font-semibold"
+                onClick={handleClick}
+            >
                 홈페이지로 이동
-            </Link>
+            </button>
         </div>
-    )
+    );
 }
